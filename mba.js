5b7ea3f321bc3308bea8983e8c19d10183bb851f
humanrgz-cpu <human.rgz@gmail.com>
2025-12-28 00:19:23 +0330

    mba.js

--- mba.js
+++ mba.js
@@ -5,12 +5,14 @@
   import { getFirestore, doc, getDoc, setDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
   // 🔧 Replace with your Firebase config
-  const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    appId: "YOUR_APP_ID"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyA1m80OnKDqLHrOyIMD3at1CblFfvh64X8",
+  authDomain: "mba-qa-46f51.firebaseapp.com",
+  projectId: "mba-qa-46f51",
+  storageBucket: "mba-qa-46f51.firebasestorage.app",
+  messagingSenderId: "554345717376",
+  appId: "1:554345717376:web:907bb2ae83e891a36ed065"
+};
 
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
@@ -371,4 +373,5 @@ function initApp() {
   });
 }
 </script>
+
 
