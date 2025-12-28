0f17b851913809265b5f335c830062a973165e36
humanrgz-cpu <human.rgz@gmail.com>
2025-12-28 11:50:48 +0330

    mba.js

--- mba.js
+++ mba.js
@@ -105,7 +105,7 @@ function loadQAData() {
   catch { return []; }
 }
 function saveQAData(data) { localStorage.setItem("qaData", JSON.stringify(data)); }
-sc = s => String(s).replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
+
 
 // ===== Section helpers & item rendering =====
 function ensureSection(category) {
@@ -433,6 +433,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
 
 
 
